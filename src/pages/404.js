1c20be486f5f6b541533deb965b8d30715eaa670
mbalazy <p@mbalazy.pl>
2020-04-28 15:13:12 +0200

    add simple tour site and finish install netlify

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const errorPage = () => {
+  return <div>there is no page with this url</div>
+}
+
+export default errorPage
---
+++ src/pages/tours.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const tours = () => {
+  return (
+    <div>
+      hallo and
+      <p>welcolaksnd me</p>
+      <p>welcolaksnd me</p>
+    </div>
+  )
+}
+
+export default tours
